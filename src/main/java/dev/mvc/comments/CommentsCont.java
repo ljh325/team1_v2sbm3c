77f4b101f0b7e06b7b2f0a22eb5a6d463177ef82
ljh325 <dlwogh325@naver.com>
2024-06-04 18:27:18 +0900

    댓글 대댓글 적용 완료

--- src/main/java/dev/mvc/comments/CommentsCont.java
+++ src/main/java/dev/mvc/comments/CommentsCont.java
@@ -45,9 +45,6 @@ public class CommentsCont {
   public CommentsCont() {
     System.out.println("-> CommentsCont Created");
   }
-  
-  
-  
 
   /**
    * 댓글 생성
@@ -59,110 +56,163 @@ public class CommentsCont {
    */
   @PostMapping(value = "/create")
   @ResponseBody
-  public String create(HttpSession session, @RequestBody CommentsVO commentsVO ) {
+  public String create(HttpSession session, @RequestBody CommentsVO commentsVO) {
     int memberno = (int) session.getAttribute("memberno"); // adminno FK
     commentsVO.setMemberno(memberno);
 
     int cnt = this.commentsProc.create(commentsVO);
     JSONObject json = new JSONObject();
     json.put("res", cnt);
-    System.out.println("res->>: " + json.toString());
     return json.toString();
 
   }
 
   /**
-  컨텐츠별 댓글 목록 
-  {
-  "list":[
-  {"memberno":2,"rdate":"2024-05-31 15:54:51","replyno":6,"id":"admin","content":"675","contentsno":12},
-  {"memberno":2,"rdate":"2024-05-31 15:53:39","replyno":5,"id":"admin","content":"456","contentsno":12},
-  {"memberno":2,"rdate":"2024-05-31 13:05:00","replyno":4,"id":"admin","content":"대기업의 쪼개기 지분 상장","contentsno":12}
-  ]
-  }
-  * http://localhost:9093/comments/list_by_contentsno_join?contentsno=30
-  * @param contentsno
-  * @return
-  */
- @ResponseBody
- @GetMapping(value="/list_by_contentsno_join")
- public String list_by_contentsno_join(int contentsno) {
-   // String msg="JSON 출력";
-   // return msg;
-   
-   ArrayList<CommentsMemberVO> list = this.commentsProc.comment_list(contentsno);
-   
-   JSONObject obj = new JSONObject();
-   obj.put("res", list);
-   
-   System.out.println("-> obj.toString(): " + obj.toString());
-
-   return obj.toString();     
- }
-
-  /**
-   * 댓글 수정
+   * 컨텐츠별 댓글 목록 { "list":[ {"memberno":2,"rdate":"2024-05-31
+   * 15:54:51","replyno":6,"id":"admin","content":"675","contentsno":12},
+   * {"memberno":2,"rdate":"2024-05-31
+   * 15:53:39","replyno":5,"id":"admin","content":"456","contentsno":12},
+   * {"memberno":2,"rdate":"2024-05-31
+   * 13:05:00","replyno":4,"id":"admin","content":"대기업의 쪼개기 지분
+   * 상장","contentsno":12} ] }
+   * http://localhost:9093/comments/list_by_contentsno_join?contentsno=30
    * 
-   * @param session
-   * @param model
-   * @param commentsno
-   * @param ra
-   * @return
-   */
-  @GetMapping(value = "/update")
-  public String update(HttpSession session, Model model, int commentsno, int contentsno, int now_page, String word) {
-
-    CommentsMemberVO commentsMemberVO = this.commentsProc.read(commentsno);
-    model.addAttribute("commentsVO", commentsMemberVO);
-    model.addAttribute("word", word);
-    model.addAttribute("now_page", now_page);
-    
-    return "comments/update";
-  }
-
-  /**
-   * 댓글 수정 Proc
-   * @param session
-   * @param model
-   * @param ra
-   * @param commentsVO
    * @param contentsno
    * @return
    */
-  @PostMapping(value = "/update")
-  public String update(HttpSession session, Model model, RedirectAttributes ra, CommentsVO commentsVO,
-                              int contentsno, int now_page, String word) {
-    
-    
-    int cnt = this.commentsProc.update(commentsVO); // 글수정
-    
-    ra.addAttribute("contentsno", contentsno);
-    ra.addAttribute("word", word);
-    ra.addAttribute("now_page", now_page);
+  @ResponseBody
+  @GetMapping(value = "/list_by_contentsno_join")
+  public String list_by_contentsno_join(int contentsno) {
+    // String msg="JSON 출력";
+    // return msg;
 
-    return "redirect:/contents/read"; // 페이지 자동 이동
+    ArrayList<CommentsMemberVO> list = this.commentsProc.comment_list(contentsno);
 
+    JSONObject obj = new JSONObject();
+    obj.put("res", list);
+
+    return obj.toString();
   }
   
   /**
-   * 댓글 삭제
+   * 댓글 조회
    * 
+   * @param replyno
    * @return
    */
-  @GetMapping(value = "/delete")
-  public String delete(RedirectAttributes ra, Model model , int commentsno, int contentsno, int now_page, String word) {
-    
-   
-    System.out.println("---->> commentsno: " + commentsno);
-    int cnt = this.commentsProc.delete(commentsno);
+  @ResponseBody
+  @GetMapping(value = "/read", produces = "application/json")
+  public String read(int commentsno) {
 
-    ra.addAttribute("contentsno", contentsno);
-    ra.addAttribute("word", word);
-    ra.addAttribute("now_page", now_page);
+    CommentsMemberVO commentsMemberVO = this.commentsProc.read(commentsno);
+//   -> obj.toString(): {"res":"ReplyVO(replyno=41, contentsno=12, memberno=2, content=댓글 수정, rdate=2024-06-03 13:21:24)"}
+    JSONObject row = new JSONObject();
 
-    return "redirect:/contents/read";
+    row.put("commentsno", commentsMemberVO.getCommentsno());
+    row.put("contentsno", commentsMemberVO.getContentsno());
+    row.put("memberno", commentsMemberVO.getMemberno());
+    row.put("contents", commentsMemberVO.getContents());
+    row.put("rdate", commentsMemberVO.getRdate());
+    row.put("replycnt", commentsMemberVO.getReplycnt());
+    row.put("id", commentsMemberVO.getId());
+    row.put("thumbs", commentsMemberVO.getThumbs());
+    row.put("grade", commentsMemberVO.getGrade());
 
+    JSONObject obj = new JSONObject();
+    obj.put("res", row);
+
+    return obj.toString();
   }
-  
-  
+
+//  /**
+//   * 댓글 수정
+//   * 
+//   * @param session
+//   * @param model
+//   * @param commentsno
+//   * @param ra
+//   * @return
+//   */
+//  @GetMapping(value = "/update")
+//  public String update(HttpSession session, Model model, int commentsno, int contentsno, int now_page, String word) {
+//
+//    CommentsMemberVO commentsMemberVO = this.commentsProc.read(commentsno);
+//    model.addAttribute("commentsVO", commentsMemberVO);
+//    model.addAttribute("word", word);
+//    model.addAttribute("now_page", now_page);
+//    
+//    return "comments/update";
+//  }
+//
+//  /**
+//   * 댓글 수정 Proc
+//   * @param session
+//   * @param model
+//   * @param ra
+//   * @param commentsVO
+//   * @param contentsno
+//   * @return
+//   */
+//  @PostMapping(value = "/update")
+//  public String update(HttpSession session, Model model, RedirectAttributes ra, CommentsVO commentsVO,
+//                              int contentsno, int now_page, String word) {
+//    
+//    
+//    int cnt = this.commentsProc.update(commentsVO); // 글수정
+//    
+//    ra.addAttribute("contentsno", contentsno);
+//    ra.addAttribute("word", word);
+//    ra.addAttribute("now_page", now_page);
+//
+//    return "redirect:/contents/read"; // 페이지 자동 이동
+//
+//  }
+
+  /**
+   * 댓글 수정 처리
+   * 
+   * @param replyVO
+   * @return
+   */
+  @PostMapping(value = "/update")
+  @ResponseBody
+  public String update(HttpSession session, @RequestBody CommentsVO commentsVO) {
+
+    int memberno = (int) session.getAttribute("memberno"); // 보안성 향상
+
+    int cnt = 0;
+    if (memberno == commentsVO.getMemberno()) { // 회원 자신이 쓴 글만 수정 가능
+
+      cnt = this.commentsProc.update(commentsVO);
+    }
+    JSONObject json = new JSONObject();
+    json.put("res", cnt); // 1: 성공, 0: 실패
+
+    return json.toString();
+  }
+
+/**
+ * 댓글 삭제 처리
+ * @param session
+ * @param commentsVO
+ * @return
+ */
+  @PostMapping(value = "/delete")
+  @ResponseBody
+  public String delete(HttpSession session, @RequestBody CommentsVO commentsVO) {
+    System.out.println("-> 삭제할 수신 데이터: " + commentsVO.toString());
+
+    int memberno = (int) session.getAttribute("memberno"); // 보안성 향상
+
+    int cnt = 0;
+    if (memberno == commentsVO.getMemberno()) { // 회원 자신이 쓴 글만 수정 가능
+
+      cnt = this.commentsProc.delete(commentsVO.getCommentsno());
+    }
+    JSONObject json = new JSONObject();
+    json.put("res", cnt); // 1: 성공, 0: 실패
+
+    return json.toString();
+  }
+
 }
--- src/main/java/dev/mvc/reply/ReplyCont.java
+++ src/main/java/dev/mvc/reply/ReplyCont.java
@@ -88,9 +88,9 @@ public class ReplyCont {
    */
   @PostMapping(value = "/create")
   @ResponseBody
-  public String create( @RequestBody ReplyVO replyVO ) {
-//    int memberno = (int) session.getAttribute("memberno"); // adminno FK
-//    replyVO.setMemberno(memberno);
+  public String create(HttpSession session, @RequestBody ReplyVO replyVO ) {
+    int memberno = (int) session.getAttribute("memberno"); // adminno FK
+    replyVO.setMemberno(memberno);
 
     int cnt = this.replyProc.create(replyVO);
     int reply_cnt = this.commentsProc.increase_replycnt(replyVO.getCommentsno());
@@ -103,14 +103,6 @@ public class ReplyCont {
   
   /**
   댓글별 대댓글 목록 
-  {
-  "list":[
-  {"memberno":2,"rdate":"2024-05-31 15:54:51","replyno":6,"id":"admin","content":"675","contentsno":12},
-  {"memberno":2,"rdate":"2024-05-31 15:53:39","replyno":5,"id":"admin","content":"456","contentsno":12},
-  {"memberno":2,"rdate":"2024-05-31 13:05:00","replyno":4,"id":"admin","content":"대기업의 쪼개기 지분 상장","contentsno":12}
-  ]
-  }
-  * http://localhost:9093/reply/list_by_commentsno_join?commentsno=3
   * @param commentsno
   * @return
   */
@@ -128,79 +120,135 @@ public class ReplyCont {
 
    return obj.toString();     
  }
-  
+ 
+ /**
+  * 대댓글 조회
+  * 
+  * @param replyno
+  * @return
+  */
+ @ResponseBody
+ @GetMapping(value = "/read", produces = "application/json")
+ public String read(int replyno) {
 
-  /**
-   * 대댓글 수정
-   * @param session
-   * @param model
-   * @param commentsno
-   * @param contentsno
-   * @param replyno
-   * @param memberno
-   * @return
-   */
-  @GetMapping(value = "/update")
-  public String update(HttpSession session, Model model, int commentsno, int contentsno, int replyno, int now_page, String word) {
-
-    CommentsMemberVO commentsMemberVO = this.commentsProc.read(commentsno);
-    model.addAttribute("commentsVO", commentsMemberVO);
-    
-    ReplyMemberVO replyMemberVO = this.replyProc.read(replyno);
-    model.addAttribute("replyVO", replyMemberVO);
-    
-    model.addAttribute("word", word);
-    model.addAttribute("now_page", now_page);
-    
-    return "reply/update";
-  }
-
-  /**
-   * 대댓글 수정 Proc
-   * @param session
-   * @param model
-   * @param ra
-   * @param commentsVO
-   * @param contentsno
-   * @return
-   */
-  @PostMapping(value = "/update")
-  public String update(HttpSession session, Model model, RedirectAttributes ra, ReplyVO replyVO,
-                              int contentsno, int commentsno, int memberno, int now_page, String word) {
-    
-    
-    this.replyProc.update(replyVO); // 글수정
-
-    ra.addAttribute("contentsno", contentsno);
-    ra.addAttribute("commentsno", commentsno);
-    ra.addAttribute("memberno", memberno);
-    ra.addAttribute("word", word);
-    ra.addAttribute("now_page", now_page);
-
-    return "redirect:/reply/create";
-
-  }
-  
-  /**
-   * 대댓글 삭제
-   * 
-   * @return
-   */
-  @GetMapping(value = "/delete")
-  public String delete(RedirectAttributes ra, Model model , int replyno, int contentsno, int commentsno, int memberno, int now_page, String word) {
-    
+   ReplyMemberVO replyMemberVO = this.replyProc.read(replyno);
+   JSONObject row = new JSONObject();
    
-    this.replyProc.delete(replyno);
+   row.put("replyno", replyMemberVO.getReplyno());
+   row.put("commentsno", replyMemberVO.getCommentsno());
+   row.put("contentsno", replyMemberVO.getContentsno());
+   row.put("memberno", replyMemberVO.getMemberno());
+   row.put("contents", replyMemberVO.getContents());
+   row.put("rdate", replyMemberVO.getRdate());
+   row.put("id", replyMemberVO.getId());
+   row.put("thumbs", replyMemberVO.getThumbs());
+   row.put("grade", replyMemberVO.getGrade());
 
-    ra.addAttribute("contentsno", contentsno);
-    ra.addAttribute("commentsno", commentsno);
-    ra.addAttribute("memberno", memberno);
-    ra.addAttribute("word", word);
-    ra.addAttribute("now_page", now_page);
+   JSONObject obj = new JSONObject();
+   obj.put("res", row);
 
-    return "redirect:/reply/create";
+   return obj.toString();
+ }
+  
 
-  }
+//  /**
+//   * 대댓글 수정
+//   * @param session
+//   * @param model
+//   * @param commentsno
+//   * @param contentsno
+//   * @param replyno
+//   * @param memberno
+//   * @return
+//   */
+//  @GetMapping(value = "/update")
+//  public String update(HttpSession session, Model model, int commentsno, int contentsno, int replyno, int now_page, String word) {
+//
+//    CommentsMemberVO commentsMemberVO = this.commentsProc.read(commentsno);
+//    model.addAttribute("commentsVO", commentsMemberVO);
+//    
+//    ReplyMemberVO replyMemberVO = this.replyProc.read(replyno);
+//    model.addAttribute("replyVO", replyMemberVO);
+//    
+//    model.addAttribute("word", word);
+//    model.addAttribute("now_page", now_page);
+//    
+//    return "reply/update";
+//  }
+//
+//  /**
+//   * 대댓글 수정 Proc
+//   * @param session
+//   * @param model
+//   * @param ra
+//   * @param commentsVO
+//   * @param contentsno
+//   * @return
+//   */
+//  @PostMapping(value = "/update")
+//  public String update(HttpSession session, Model model, RedirectAttributes ra, ReplyVO replyVO,
+//                              int contentsno, int commentsno, int memberno, int now_page, String word) {
+//    
+//    
+//    this.replyProc.update(replyVO); // 글수정
+//
+//    ra.addAttribute("contentsno", contentsno);
+//    ra.addAttribute("commentsno", commentsno);
+//    ra.addAttribute("memberno", memberno);
+//    ra.addAttribute("word", word);
+//    ra.addAttribute("now_page", now_page);
+//
+//    return "redirect:/reply/create";
+//
+//  }
+ 
+ /**
+  * 대댓글 수정 처리
+  * 
+  * @param replyVO
+  * @return
+  */
+ @PostMapping(value = "/update")
+ @ResponseBody
+ public String update(HttpSession session, @RequestBody ReplyVO replyVO) {
+
+   int memberno = (int) session.getAttribute("memberno"); // 보안성 향상
+
+   int cnt = 0;
+   if (memberno == replyVO.getMemberno()) { // 회원 자신이 쓴 글만 수정 가능
+
+     cnt = this.replyProc.update(replyVO);
+   }
+   JSONObject json = new JSONObject();
+   json.put("res", cnt); // 1: 성공, 0: 실패
+
+   return json.toString();
+ }
+  
+ /**
+  * 댓글 삭제 처리
+  * @param session
+  * @param commentsVO
+  * @return
+  */
+   @PostMapping(value = "/delete")
+   @ResponseBody
+   public String delete(HttpSession session, @RequestBody ReplyVO replyVO) {
+
+     int memberno = (int) session.getAttribute("memberno"); // 보안성 향상
+
+     int cnt = 0;
+     if (memberno == replyVO.getMemberno()) { // 회원 자신이 쓴 글만 수정 가능
+
+       cnt = this.replyProc.delete(replyVO.getReplyno());
+       this.commentsProc.decrease_replycnt(replyVO.getCommentsno());
+
+     }
+     JSONObject json = new JSONObject();
+     json.put("res", cnt); // 1: 성공, 0: 실패
+
+     return json.toString();
+   }
   
   /**
    * 대댓글 전체 삭제
